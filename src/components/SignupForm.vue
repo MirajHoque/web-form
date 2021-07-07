0ac3f8aa452fd965acdb722356b900a75921c0da
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-08 00:07:57 +0600

    checkbox

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -7,9 +7,38 @@
     <label >Password:</label>
     <input type="password" required v-model="password">
 
+    <label >Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <div class="terms">
+      <input type="checkbox" required v-model="terms">
+      <label>Accept terms and conditions</label>
+    </div>
+
+    <div>
+      <input type="checkbox" value="Shaun" v-model="names">
+      <label>Shaun</label>
+    </div>
+    
+    <div>
+      <input type="checkbox" value="Yoshi" v-model="names">
+      <label>Yoshi</label>
+    </div>
+    
+    <div>
+      <input type="checkbox" value="Mario" v-model="names">
+      <label>Mario</label>
+    </div>
+
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms accepted {{ terms }}</p>
+  <p>Names: {{ names }}</p>
 </template>
 
 <script>
@@ -17,7 +46,10 @@ export default {
    data(){
     return{
       email: '',
-      password: ''
+      password: '',
+      role: 'designer',
+      terms: false,
+      names: []
     }
   }
 
@@ -42,7 +74,7 @@ label{
   letter-spacing: 1px;
   font-weight: bold;
 }
-input{
+input, select{
   display: block;
   padding: 10px 60px;
   width: 100%;
@@ -51,4 +83,12 @@ input{
   border-bottom: 1px solid #ddd;
   color: #555;
 }
+input[type="checkbox"]{
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+
 </style>
