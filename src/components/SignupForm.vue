47234e1a46d5772770e553b8ef140703f75af81c
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-08 22:25:45 +0600

    form submission

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,11 +1,12 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
 
     <label >Email:</label>
     <input type="email" required v-model="email">
 
     <label >Password:</label>
     <input type="password" required v-model="password">
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label >Role:</label>
     <select v-model="role">
@@ -26,6 +27,10 @@
       <label>Accept terms and conditions</label>
     </div>
 
+    <div class="submit">
+      <button>Create an account</button>
+    </div>
+
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
@@ -42,7 +47,8 @@ export default {
       role: 'designer',
       terms: false,
       tempSkill: '',
-      skills: []
+      skills: [],
+      passwordError: ''
     }
   },
   methods:{
@@ -59,6 +65,17 @@ export default {
       this.skills= this.skills.filter((item)=>{
         return element !==item;
       })
+    },
+    handleSubmit(){
+      //validate password
+      this.passwordError= this.password.length>5 ? '' : 'password at least 5 charecters';
+      if(!this.passwordError){
+        console.log('email', this.email);
+        console.log('password', this.password);
+        console.log('role', this.role);
+        console.log('skills', this.skills);
+        console.log('terms accepted', this.terms)
+      }
     }
   }
 
@@ -111,5 +128,21 @@ input[type="checkbox"]{
   color: #777;
   cursor: pointer;
 }
-
+button{
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20;
+  color: white;
+  border-radius: 20px;
+}
+.submit{
+  text-align: center;
+}
+.error{
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
 </style>
